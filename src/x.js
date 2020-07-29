79497c3c372cd500b362bbc43c3b7082722dfbc2
ddb324 <jiangdaoran@163.com>
2020-07-29 17:06:35 +0800

    update

---
+++ src/x.js
@@ -0,0 +1,3 @@
+import css from "./x.css";
+console.log(css.toString());
+export default "xxx";
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
@@ -6,5 +7,19 @@ module.exports = {
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ddb",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
