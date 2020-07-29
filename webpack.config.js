75cb3c2c3c7ceabf88b48e45a1f2c96a79f173ad
ddb324 <jiangdaoran@163.com>
2020-07-29 17:57:35 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,15 @@ var HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
   plugins: [
     new HtmlWebpackPlugin({
